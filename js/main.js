03b2bd5d5acf8979462ef8113a6dbf1dd0d81164
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-06 20:53:52 +0300

    little correction

--- js/main.js
+++ js/main.js
@@ -14,6 +14,95 @@ class FilterCards {
     this.listeners();
   }
 
+  getData(url, cb) {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState !== 4) return;
+      if (request.status === 200) {
+        const response = JSON.parse(request.responseText);
+        cb(response);
+      } else {
+        new Error(request.statusText);
+      }
+    });
+
+    request.open('GET', url);
+    // request.setRequestHeader('Content-Type', 'application/json');
+    request.send();
+  }
+
+  getValues(heroes, key) {
+    const values = new Set();
+    heroes.forEach(item => {
+      const arrOfHeroesWithKey = this.projection(key, item);
+      if (arrOfHeroesWithKey[key]) {
+        arrOfHeroesWithKey[key].forEach(item => values.add(item));
+      }
+
+    });
+
+    return values;
+  }
+
+  projection(fields, obj) {
+    return Object.keys(obj).filter(field => fields.includes(field)).reduce((mewObj, key) => {
+      mewObj[key] = obj[key];
+      return mewObj;
+    }, {});
+  }
+
+
+  renderCheckbox(textArr) {
+    this.select.textContent = '';
+    textArr.forEach(item => {
+      const box = document.createElement('div');
+      box.classList.add('heroes-checkbox');
+      box.innerHTML = `
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  id="checkbox"
+                  value="${item}"
+                />
+                <label for="checkbox">${item}</label>
+                `;
+      this.select.append(box);
+    });
+  }
+
+  renderCards(cards) {
+    this.cardsWrapper.textContent = '';
+
+    cards.forEach(cardData => {
+      const card = document.createElement('div');
+      card.classList.add('heroes-card');
+      card.innerHTML = `
+              <img src="${cardData.photo.replace(/\/$/, '')}"
+              alt=""
+              class="heroes-bckg"
+            />
+            <span class="heroes-name">${cardData.name}</span>
+            <div class="heroes-propertie-block">
+              
+            </div>
+            `;
+      const cardDataBlock = card.querySelector('.heroes-propertie-block');
+
+      for (let key in cardData) {
+        if (key !== 'name' && key !== 'photo') {
+          const p = document.createElement('p');
+          p.classList.add('heroes-propertie');
+          p.innerHTML = `<span>${key.toUpperCase()} </span> <span>${cardData[key]}</span>`;
+
+          cardDataBlock.append(p);
+        }
+      }
+
+      this.cardsWrapper.append(card);
+    });
+  }
+
   reDrowWithFilter(filter, key) {
     this.getData('./dbHeroes.json', data => {
       const filteredData = data.filter(obj => {
@@ -28,7 +117,6 @@ class FilterCards {
 
       if (filteredData.length) {
         this.renderCards(filteredData);
-        console.log(filteredData);
       } else {
         const emptyObj = [{
           name: 'UNIVERSAL',
@@ -52,98 +140,6 @@ class FilterCards {
         this.reDrowWithFilter(this.selectedKeys, 'movies');
       }
 
-    });
-  }
-
-  renderCheckbox(textArr) {
-    this.select.textContent = '';
-
-    textArr.forEach(item => {
-      const box = document.createElement('div');
-      box.classList.add('heroes-checkbox');
-      box.innerHTML = `
-                <input
-                  type="checkbox"
-                  class="checkbox"
-                  id="checkbox"
-                  value="${item}"
-                />
-                <label for="checkbox">${item}</label>
-`;
-
-      this.select.append(box);
-    });
-
-
-  }
-
-  getValues(heroes, key) {
-    const values = new Set();
-    heroes.forEach(item => {
-      const arrOfHeroesWithKey = this.projection(key, item);
-      if (arrOfHeroesWithKey[key]) {
-        arrOfHeroesWithKey[key].forEach(item => values.add(item));
-      }
-
-    });
-
-    return values;
-  }
-
-  projection(fields, obj) {
-    return Object.keys(obj).filter(field => fields.includes(field)).reduce((mewObj, key) => {
-      mewObj[key] = obj[key];
-      return mewObj;
-    }, {});
-  }
-
-  getData(url, cb) {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState !== 4) return;
-      if (request.status === 200) {
-        const response = JSON.parse(request.responseText);
-        cb(response);
-      } else {
-        new Error(request.statusText);
-      }
-    });
-
-    request.open('GET', url);
-    // request.setRequestHeader('Content-Type', 'application/json');
-    request.send();
-  }
-
-  renderCards(cards) {
-    this.cardsWrapper.textContent = '';
-
-    cards.forEach(cardData => {
-      const card = document.createElement('div');
-      card.classList.add('heroes-card');
-      card.innerHTML = `
-              <img src="${cardData.photo}"
-              alt=""
-              class="heroes-bckg"
-            />
-            <span class="heroes-name">${cardData.name}</span>
-            <div class="heroes-propertie-block">
-              
-            </div>
-            `;
-      const cardDataBlock = card.querySelector('.heroes-propertie-block');
-
-      for (let key in cardData) {
-        if (key !== 'name' && key !== 'photo') {
-          const p = document.createElement('p');
-          p.classList.add('heroes-propertie');
-          p.innerHTML = `<span>${key.toUpperCase()} </span> <span>${cardData[key]}</span>`;
-
-          cardDataBlock.append(p);
-        }
-      }
-
-      this.cardsWrapper.append(card);
     });
   }
 }
